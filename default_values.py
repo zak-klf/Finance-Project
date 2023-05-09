c16d7cbde8281a1e869e3d109688357887735538
kaz-hk <khelif.zakarya@protonmail.com>
2023-05-09 16:34:23 +0200

    second commit, added first Jupyter Notebooks containing the basics

---
+++ default_values.py
@@ -0,0 +1,15 @@
+import datetime as dt
+
+S_YEAR = 2017
+S_MONTH = 1
+S_DAY = 3
+S_DATE_STR = f"{S_YEAR}-{S_MONTH}-{S_DAY}"
+
+S_DATE_DATETIME = dt.datetime(S_YEAR, S_MONTH, S_DAY)
+
+E_YEAR = 2021
+E_MONTH = 8
+E_DAY = 19
+E_DATE_STR = f"{E_YEAR}-{E_MONTH}-{E_DAY}"
+
+E_DATE_DATETIME = dt.datetime(E_YEAR, E_MONTH, E_DAY)
